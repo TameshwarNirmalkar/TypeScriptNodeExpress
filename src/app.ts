0d73e56f75247a2df6086d7942772ebbfa2ea4fe
Tameshwar Nirmalkar <tameshwar.nirmalkar@scrumstart.net>
2019-05-20 15:36:28 +0530

    fix issues ts

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,35 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as logger from 'morgan';
+import * as cors from 'cors';
+
 
 class App {
 
-    public app: express.Application;
+  public app: express.Application;
 
-    constructor() {
-        this.app = express();
-        this.config();        
-    }
+  constructor() {
+    this.app = express();
+    this.config();
+  }
 
-    private config(): void{
-        // support application/json type post data
-        this.app.use(bodyParser.json());
-        //support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-    }
+  private config(): void {
+    const options: cors.CorsOptions = {
+      allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+      credentials: true,
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      origin: '*',
+      preflightContinue: false
+    };
+    this.app.use(cors(options));
+    this.app.options('*', cors(options));
+    this.app.use(logger('dev'));
+    this.app.use(bodyParser.urlencoded({
+      limit: '150mb',
+      extended: true
+    }));
+    this.app.use(bodyParser.json({ limit: '50mb' }));
+  }
 
 }
 
